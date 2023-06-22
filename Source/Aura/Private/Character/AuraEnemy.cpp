c83fcbe04a7a831c87019ea577c88417568df68c
Chelsey <chelseysmith.mail@gmail.com>
2023-06-21 21:44:45 -0600

    Section 13: Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,35 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	if (Weapon)
+	{
+		Weapon->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	ToggleHighlight(true);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	ToggleHighlight(false);
+}
+
+void AAuraEnemy::ToggleHighlight(bool bIsEnabled)
+{
+	GetMesh()->SetRenderCustomDepth(bIsEnabled);
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(bIsEnabled);
+	}
 }
