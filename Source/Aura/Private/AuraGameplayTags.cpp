a1f8ddc63ee77484a86602da78f4967f76b5f4c0
Chelsey <chelseysmith.mail@gmail.com>
2023-09-03 10:41:34 -0600

    Section 20.10: Gameplay Ability Cooldown

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -206,6 +206,15 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 
 
 	/*
+	 * Cooldown
+	 */
+	GameplayTags.Cooldown_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Cooldown.Fire.FireBolt"),
+		FString("FireBolt Cooldown Tag")
+		);
+
+
+	/*
 	 * Combat Socket
 	 */
 	GameplayTags.CombatSocket_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
